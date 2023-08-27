a14c4de72ab8637d37629350801d18d4356ad9c2
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-08-27 12:29:57 -0400

    final touches and debugging

--- Develop/Client/src/js/database.js
+++ Develop/Client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction(['jate'], 'readwrite');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.add({ content: content });
+
+  const result = await request;
+  console.log('Data saved to database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (content) => {
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction(['jate'], 'readonly');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
